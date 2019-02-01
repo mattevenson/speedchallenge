1ceda044b0e41ec3528074f42d22fc6b08430cba
Matt Evenson <mevenson@usc.edu>
2019-02-01 12:31:33 -0800

    Fix data generating scripts

--- flow.sh
+++ flow.sh
@@ -5,32 +5,24 @@ mkdir data/test/flow
 
 cd flownet2-pytorch
 
-python main.py --inference --model FlowNet2 --save_flow --save ../data/train/flow --inference_dataset ImagesFromFolder --inference_dataset_root ../data/train/raw --resume ../models/FlowNet2_checkpoint.pth.tar
-python main.py --inference --model FlowNet2 --save_flow --save ../data/test/flow --inference_dataset ImagesFromFolder --inference_dataset_root ../data/test/raw --resume ../models/FlowNet2_checkpoint.pth.tar
+python main.py --inference --inference_batch_size 4 --model FlowNet2 --save_flow --save ../data/train/flow --inference_dataset ImagesFromFolder --inference_dataset_root ../data/train/raw --resume ../models/FlowNet2_checkpoint.pth.tar
+python main.py --inference --inference_batch_size 4 --model FlowNet2 --save_flow --save ../data/test/flow --inference_dataset ImagesFromFolder --inference_dataset_root ../data/test/raw --resume ../models/FlowNet2_checkpoint.pth.tar
 
 cd ..
 
 ./flow-io-opencv/cli/cli data/train/flow/inference/run.epoch-0-flow-field data/train/flow --vis-dir data/train/flow
-./flow-io-opencv/cli/cli data/test/flow/inference/run.epoch-0-flow-field data/train/flow --vis-dir data/test/flow
 
 rm data/train/flow/*.txt
+rm data/train/flow/arg.txt
 rm -rf data/train/flow/inference
 rm -rf data/train/flow/train
 rm -rf data/train/flow/validation
-rm data/train/flow/arg.txt
 
-for file in data/train/flow/*.flo.png; do
-    mv $file "${data/train/flow/file%.flo.png}.png";
-done
+./flow-io-opencv/cli/cli data/test/flow/inference/run.epoch-0-flow-field data/train/flow --vis-dir data/test/flow
 
 rm data/test/flow/*.txt
+rm data/test/flow/arg.txt
 rm -rf data/test/flow/inference
 rm -rf data/test/flow/train
 rm -rf data/test/flow/validation
-rm data/test/flow/arg.txt
-
-for file in data/test/flow/*.flo.png; do
-    mv $file "${data/test/flow/file%.flo.png}.png";
-done
-
 
--- raw.sh
+++ raw.sh
@@ -3,7 +3,7 @@
 mkdir data/train data/test
 
 mkdir data/train/raw
-ffmpeg -i data/train.mp4 -vf "crop=640:320:0:40, scale=512:256" data/train/raw/%05d.png
+ffmpeg -i data/train.mp4 -start_number 0 -vf "crop=640:320:0:40, scale=512:256" data/train/raw/%06d.png
 
 mkdir data/test/raw
-ffmpeg -i data/test.mp4 -vf "crop=640:320:0:40, scale=512:256" data/test/raw/%05d.png
+ffmpeg -i data/test.mp4 -start_number 0 -vf "crop=640:320:0:40, scale=512:256" data/test/raw/%06d.png
